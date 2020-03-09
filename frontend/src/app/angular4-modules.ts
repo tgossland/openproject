a922c57a85a9377d5fd646a4e5f53adb4ef10ba2
Inga Mai <inga.mai@outlook.de>
2020-03-09 17:24:57 +0100

    cleanup and DRY; youtube video added

--- frontend/src/app/angular4-modules.ts
+++ frontend/src/app/angular4-modules.ts
@@ -56,7 +56,8 @@ import {PasswordConfirmationModal} from "core-components/modals/request-for-conf
 import {EnterpriseBaseComponent} from "core-components/enterprise/enterprise-base.component";
 import {EnterpriseTrialModal} from "core-components/enterprise/enterprise-modal/enterprise-trial.modal";
 import {EETrialFormComponent} from "core-components/enterprise/enterprise-modal/enterprise-trial-form/ee-trial-form.component";
-import {EETrialWaitingComponent} from "core-components/enterprise/enterprise-modal/enterprise-trial-waiting/ee-trial-waiting.component";
+import {EETrialWaitingComponent} from "core-components/enterprise/enterprise-trial-waiting/ee-trial-waiting.component";
+import {EEActiveTrialComponent} from "core-components/enterprise/enterprise-active-trial/ee-active-trial.component";
 import {EnterpriseTrialService} from "core-components/enterprise/enterprise-trial.service";
 import {OpTitleService} from 'core-components/html/op-title.service';
 import {OpenprojectFieldsModule} from "core-app/modules/fields/openproject-fields.module";
@@ -184,6 +185,7 @@ import {KeyboardShortcutService} from "core-app/modules/a11y/keyboard-shortcut-s
     EnterpriseTrialModal,
     EETrialFormComponent,
     EETrialWaitingComponent,
+    EEActiveTrialComponent,
 
     // Main menu
     MainMenuResizerComponent,
@@ -212,6 +214,7 @@ import {KeyboardShortcutService} from "core-app/modules/a11y/keyboard-shortcut-s
     EnterpriseTrialModal,
     EETrialFormComponent,
     EETrialWaitingComponent,
+    EEActiveTrialComponent,
 
     // Main menu
     MainMenuResizerComponent,
